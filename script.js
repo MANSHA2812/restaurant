e04447fa6ece8a78ad3f8697ffdd7c4fdb14e759
MANSHA2812 <manshaghanchi28@gmail.com>
2025-01-02 10:48:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// checkScore = function(){
+
+//     let studentName = prompt("type Your Name");
+//     let studentScore = prompt("Type Your Score");
+    
+//     if(studentScore > 0 && studentScore < 30){
+//         document.write(studentName + " " + "You Failed");
+//     }
+    
+//     else if(studentScore > 30 && studentScore < 60){
+//         document.write(studentName+ " " + "you got third Grade");
+//     }
+    
+//     else if(studentScore > 60 && studentScore < 80){
+//         document.write(studentName + " " + "You Got Second Grade");
+//     }
+//     else if(studentScore > 80 && studentScore < 100){
+//         document.write(studentName + " " + "You Got First Grade");
+//     }
+//     else{
+//         document.write("Type Correct Value")
+//     }
+    
+// }
+
+
+
